84fdfe964001a19084034fdb7f023051942e9af4
Nithya Natarajan <nithyanatarajn@gmail.com>
2018-12-05 16:27:09 +0530

    Add support for specifying matchwith value
    Signed-off-by: Nithya Natarajan <nithyanatarajn@gmail.com>

--- replace.go
+++ replace.go
@@ -7,31 +7,52 @@ import (
 	"strings"
 )
 
+// MatcherField represents field type to be matched.
+type MatcherField string
+
+const (
+	// VALUE represents matcher field as 'value'
+	VALUE MatcherField = "value"
+)
+
 // Replacer is the struct that holds the patterns for find and replace
 type Replacer struct {
-	patterns map[string]string
+	MatchWith MatcherField
+	Patterns  Patterns
 }
 
+// Patterns represents field and value to be replaced with
+type Patterns map[string]string
+
 // New function creates a new Replacer
-func New(p map[string]string) Replacer {
-	return Replacer{patterns: p}
+func New(p Patterns, with *string) Replacer {
+	if with == nil {
+		return Replacer{MatchWith: "value", Patterns: p}
+	}
+	return Replacer{MatchWith: MatcherField(*with), Patterns: p}
 }
 
 // LoadFromFile loads a map from a Reader
 // The contents should be of the format
 // {
+//	"MatchWith": "value",
+//	"Patterns": {
 //     "pattern-1": "replace-value-1",
 //     "pattern-2": "replace-value-2"
+//	}
 // }
 func LoadFromFile(r io.Reader) (Replacer, error) {
-	var p map[string]string
+	var rr Replacer
 
-	err := json.NewDecoder(r).Decode(&p)
-	if err != nil {
-		return Replacer{}, fmt.Errorf("Error loading from file: %v", err)
+	if err := json.NewDecoder(r).Decode(&rr); err != nil {
+		return Replacer{}, fmt.Errorf("error loading from file: %v", err)
 	}
 
-	return New(p), nil
+	if rr.MatchWith != VALUE {
+		rr.MatchWith = VALUE
+	}
+
+	return rr, nil
 }
 
 // Replace uses the patters configured in the replacer and accepts a string
@@ -39,7 +60,7 @@ func LoadFromFile(r io.Reader) (Replacer, error) {
 // It also provides a bool, indicating if a change was done
 func (r *Replacer) Replace(source string) (string, bool) {
 	var changed bool
-	for key, value := range r.patterns {
+	for key, value := range r.Patterns {
 		if strings.Contains(source, key) {
 			source = strings.Replace(source, key, value, -1)
 			changed = true
--- replace_test.go
+++ replace_test.go
@@ -8,51 +8,93 @@ import (
 )
 
 func TestLoadFromFile(t *testing.T) {
-	filecontents := `{
-		"pattern-1": "value-1",
-		"pattern-2": "value-2"
-}`
-	reader := strings.NewReader(filecontents)
+	t.Run("Should match patterns with value", func(t *testing.T) {
+		filecontents := `{
+		"MatchWith": "value",
+		"Patterns": {
+			"pattern-1": "value-1",
+			"pattern-2": "value-2"
+		}
+	}`
+		reader := strings.NewReader(filecontents)
 
-	r, err := LoadFromFile(reader)
-	expected := map[string]string{
-		"pattern-1": "value-1",
-		"pattern-2": "value-2",
-	}
+		r, err := LoadFromFile(reader)
+		expected := Replacer{
+			MatchWith: "value",
+			Patterns: Patterns{
+				"pattern-1": "value-1",
+				"pattern-2": "value-2",
+			},
+		}
 
-	assert.NoError(t, err)
-	assert.Equal(t, expected, r.patterns)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, r)
 
-	invalidFilecontent := `{
+		invalidFilecontent := `{
 		notajson
 }`
-	invalidJsonReader := strings.NewReader(invalidFilecontent)
+		invalidJSONReader := strings.NewReader(invalidFilecontent)
 
-	_, err = LoadFromFile(invalidJsonReader)
+		_, err = LoadFromFile(invalidJSONReader)
 
-	assert.Error(t, err, "Should have failed parsing JSON")
+		assert.Error(t, err, "Should have failed parsing JSON")
+	})
+
+	t.Run("Should match patterns with value even if MatchWith is invalid", func(t *testing.T) {
+		filecontents := `{
+		"MatchWith": "breakMe",
+		"Patterns": {
+			"pattern-1": "value-1",
+			"pattern-2": "value-2"
+		}
+	}`
+		reader := strings.NewReader(filecontents)
+
+		r, err := LoadFromFile(reader)
+		expected := Replacer{
+			MatchWith: "value",
+			Patterns: Patterns{
+				"pattern-1": "value-1",
+				"pattern-2": "value-2",
+			},
+		}
+
+		assert.NoError(t, err)
+		assert.Equal(t, expected, r)
+
+		invalidFilecontent := `{
+		notajson
+}`
+		invalidJSONReader := strings.NewReader(invalidFilecontent)
+
+		_, err = LoadFromFile(invalidJSONReader)
+
+		assert.Error(t, err, "Should have failed parsing JSON")
+	})
 }
 
 func TestReplace(t *testing.T) {
-	patterns := map[string]string{
-		"pattern-1": "value-1",
-		"pattern-2": "value-2",
-	}
+	t.Run("Should replace values based on patterns", func(t *testing.T) {
+		patterns := Patterns{
+			"pattern-1": "value-1",
+			"pattern-2": "value-2",
+		}
 
-	r := New(patterns)
+		r := New(patterns, nil)
 
-	result, changed := r.Replace("This is pattern-1")
+		result, changed := r.Replace("This is pattern-1")
 
-	assert.True(t, changed)
-	assert.Equal(t, "This is value-1", result)
+		assert.True(t, changed)
+		assert.Equal(t, "This is value-1", result)
 
-	result, changed = r.Replace("This need not be changed")
+		result, changed = r.Replace("This need not be changed")
 
-	assert.False(t, changed)
-	assert.Equal(t, "This need not be changed", result)
+		assert.False(t, changed)
+		assert.Equal(t, "This need not be changed", result)
 
-	result, changed = r.Replace("This is pattern-1 and this is pattern-2")
+		result, changed = r.Replace("This is pattern-1 and this is pattern-2")
 
-	assert.True(t, changed)
-	assert.Equal(t, "This is value-1 and this is value-2", result)
+		assert.True(t, changed)
+		assert.Equal(t, "This is value-1 and this is value-2", result)
+	})
 }
